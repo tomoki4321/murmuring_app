75fe6a738f8675007436ed7821f8fe622651c77a
tomoki <benzousan7@gmail.com>
2022-11-25 22:55:53 +0900

    新規投稿画面を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @post=Post.all
   end
   def new
     @post=Post.new
+  end
+  def create
+    Post.create(pblog_params)
+    redirect_to new_post_path
+  end
+  private
+
+  def blog_params
+    params.require(:post).permit(:content)
   end
 end
