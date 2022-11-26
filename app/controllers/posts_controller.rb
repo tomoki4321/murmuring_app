04913545795ae48f48b7be5f60768876dc0dba41
tomoki <benzousan7@gmail.com>
2022-11-26 20:09:24 +0900

    詳細画面を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,22 @@
 class PostsController < ApplicationController
+
   def index
     @posts=Post.all
   end
+
   def new
     @post=Post.new
   end
+
   def create
     Post.create(blog_params)
     redirect_to new_post_path
   end
+
+  def show
+    @post=Post.find(params[:id])
+  end
+
   private
 
   def blog_params
