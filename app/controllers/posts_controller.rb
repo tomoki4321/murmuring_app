7abc35c15acbf8b3d4a7f228e2f8493c87d4fb97
tomoki <benzousan7@gmail.com>
2022-11-26 21:09:53 +0900

    削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts=Post.all
@@ -31,6 +31,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました！"
   end
 
   private
